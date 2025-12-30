444084a94006bb171ea19a40f90162efc2a4504f
ClaytonDewey <clay@dryadmedia.com>
2025-12-30 11:33:24 -0600

    refactor: Add forwardRef to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ type = 'button', ...props }: ButtonProps) => {
-  return <button type={type} {...props} />;
-};
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ type = 'button', ...props }, ref) => {
+    return <button type={type} ref={ref} {...props} />;
+  }
+);
+
+Button.displayName = 'Button';
 
 export default Button;
