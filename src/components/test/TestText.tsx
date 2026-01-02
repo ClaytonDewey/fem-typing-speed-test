260400a02e7d7800b6366428e44e1483a3236a7f
ClaytonDewey <clay@dryadmedia.com>
2026-01-02 14:37:30 -0600

    style: Set up basic styling for test text area

--- src/components/test/TestText.tsx
+++ src/components/test/TestText.tsx
@@ -1,4 +1,41 @@
+import { Button } from '../';
+
 const TestText = () => {
-  return <div>TestText</div>;
+  return (
+    <section className='test__body'>
+      <div className='test__overlay'>
+        <Button type='button' className='btn btn-primary'>
+          Start Typing Test
+        </Button>
+        <p>Or click the text and start typing</p>
+      </div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit sit
+        quis explicabo rem asperiores accusamus, dolorum doloribus nihil autem
+        illum, beatae quos quisquam repellat debitis et alias quam, nobis culpa.
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam modi
+        eveniet sit, non reiciendis nobis consequuntur? Modi, voluptate in
+        necessitatibus distinctio odit iste eius tenetur, eveniet tempora
+        deserunt asperiores illum! Doloribus quam sit repellendus nesciunt.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        reprehenderit dignissimos error amet suscipit a hic asperiores labore
+        omnis natus?
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et quia
+        quibusdam unde molestias fuga, exercitationem corporis earum laudantium
+        quaerat excepturi ratione delectus animi consequatur debitis nulla odit,
+        corrupti odio? Rerum consequatur nemo tempore nostrum modi, dolorum
+        fugiat aut eveniet aliquid?
+      </p>
+    </section>
+  );
 };
 export default TestText;
